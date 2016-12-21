911cb786781e845ef0d75b6975ee4d311b155aaf
Ramsundar Shandilya <ram.shandilya@ymedialabs.com>
2016-12-21 15:12:35 -0800

    updated index.js

--- index.js
+++ index.js
@@ -30,3 +30,37 @@ app.get('/webhook/', function (req, res) {
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
+
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
+
+const token = "EAAYeZAAS0RtYBAC6vZBCyucj3QfFMHUiZANYKZAoq6p1xNK4SW2Vu5j7qZBht0fr9Y2gPdoxrTnv67ZA1S17jxmSSELRczUTKxqGI9Qm4izIYbWtKcBZAauRR3bOb0ZBZCFI6MghZBDWaZBwMnCU2ZCmV9cho0CKB1Je8ZCMF6xxtfee5HQZDZD"
+
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
